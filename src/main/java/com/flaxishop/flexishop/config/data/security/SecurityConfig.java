c4d23060ed7cc99fdc2498ada016fabd3dd442d4
islam.amrouche <islemamrouche7@gmail.com>
2025-01-07 11:16:38 +0100

    Adding authentication

--- src/main/java/com/flaxishop/flexishop/config/data/security/SecurityConfig.java
+++ src/main/java/com/flaxishop/flexishop/config/data/security/SecurityConfig.java
@@ -1,30 +1,41 @@
 package com.flaxishop.flexishop.config.data.security;
 
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Keycloak security configuration
-        http
-                .csrf().disable() // Disable CSRF for stateless APIs
-                .authorizeRequests()
-                .requestMatchers("/api/**").authenticated()  // Protect API endpoints
-                .anyRequest().permitAll();  // Allow other requests
+    private final JwtAuthConverter jwtAuthConverter;
 
-        return http.build();
+    // Constructor injection for JwtAuthConverter
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
     }
 
     @Bean
-    public KeycloakAuthenticationProvider keycloakAuthenticationProvider() {
-        return new KeycloakAuthenticationProvider();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for stateless APIs
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()) // Require authentication for all endpoints
+
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthConverter))) // Use custom JWT authentication converter
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(STATELESS)); // Stateless session management
+
+        return http.build();
     }
 }
